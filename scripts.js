1806310e48ca3f04a0c6da0e2d52b8a32807f716
Raphael Ezeigwe <iraphael1308@gmail.com>
2023-11-09 20:11:42 +0100

    Add css

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", () => createBoard())
 function createBoard() {
     for (let i = 0; i < 16; i++) {
       const childRow = document.createElement("div");
-      childRow.setAttribute("class", "board-section__board--row");
+      childRow.setAttribute("class", "board-section__row");
       boardSection.appendChild(childRow);
 
       for (let j = 0; j < 16; j++) {
         const childCol = document.createElement("div");
-        childCol.setAttribute("class", "board-section__board--col");
+        childCol.setAttribute("class", "board-section__col");
         childRow.appendChild(childCol);
       }
     }
