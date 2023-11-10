67b5d65c422ad56bf55ac3e20d960d7890f7be43
Raphael Ezeigwe <iraphael1308@gmail.com>
2023-11-10 16:25:19 +0100

    Add draw

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
 const boardSection = document.getElementById("board_section")
+const pixels = document.getElementsByClassName("pixel")
 
 document.addEventListener("DOMContentLoaded", () => createBoard())
 
 function createBoard() {
-    for (let i = 0; i < 16; i++) {
-      const childRow = document.createElement("div");
-      childRow.setAttribute("class", "board-section__row");
-      boardSection.appendChild(childRow);
+  for (let i = 0; i < 16; i++) {
+    const childRow = document.createElement("div");
+    childRow.setAttribute("class", "board-section__row");
+    boardSection.appendChild(childRow);
 
-      for (let j = 0; j < 16; j++) {
-        const childCol = document.createElement("div");
-        childCol.setAttribute("class", "board-section__col");
-        childRow.appendChild(childCol);
-      }
+    for (let j = 0; j < 16; j++) {
+      const childCol = document.createElement("div");
+      childCol.setAttribute("class", "board-section__col pixel");
+      childRow.appendChild(childCol);
     }
+  }
 
-    console.log(boardSection)
+  draw()
+}
+
+function draw() {
+  for (let element of pixels) {
+    element.addEventListener("click", (e) => {e.target.style.backgroundColor = "crimson"})
+  }
 }
